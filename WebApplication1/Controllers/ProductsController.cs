8380c90341eeb6a494ab8f589b558331cddc8b05
unknown <e0046644@ISSBOR31302.iss.nus.edu.sg>
2016-11-20 11:56:16 +0800

    json+EF

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace WebApplication1.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Team6PropertyDBEntities propertymodel = new Team6PropertyDBEntities();
+        public List<Property> GetAllProducts()
+        {
+            return propertymodel.Properties.ToList<Property>();
+        }
+
+        public IHttpActionResult GetProperty(int id)
+        {
+            var property = propertymodel.Properties.FirstOrDefault((p) => p.PropertyID == id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+            return Ok(property);
+        }
+    }
+}
